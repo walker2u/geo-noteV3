2247b71d828c6959ef4945a23cee038fbd215d3f
walker2u <kumar99prasad@gmail.com>
2025-11-21 21:32:04 +0530

    ts to js

--- src/app/.well-known/farcaster.json/route.js
+++ src/app/.well-known/farcaster.json/route.js
@@ -1,6 +1,4 @@
-function withValidProperties(
-  properties: Record<string, undefined | string | string[]>
-) {
+function withValidProperties(properties) {
   return Object.fromEntries(
     Object.entries(properties).filter(([_, value]) =>
       Array.isArray(value) ? value.length > 0 : !!value
@@ -9,7 +7,7 @@ function withValidProperties(
 }
 
 export async function GET() {
-  const URL = process.env.NEXT_PUBLIC_URL as string;
+  const URL = process.env.NEXT_PUBLIC_URL;
   return Response.json({
     accountAssociation: {
       // these will be added in step 5
